723a1a740e76f9c4d454185d61dd366f86daf714
Hungchen Fu <sean14xx@gmail.com>
2020-11-12 11:22:29 +0100

    feat: Add basic UI to AddEntry

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { View } from 'react-native'
 import { getMetricMetaInfo } from '../utils/helpers'
+import Slider from './Slider'
+import Steppers from './Steppers'
 
 export default class AddEntry extends React.Component {
     state = {
@@ -10,9 +12,10 @@ export default class AddEntry extends React.Component {
         sleep: 0,
         eat: 0,
     }
+
     increment = (metric) => {
         const { max, step } = getMetricMetaInfo(metric)
-        
+
         this.setState((state) => {
             const count = state[metric] + step
 
@@ -22,7 +25,8 @@ export default class AddEntry extends React.Component {
             }
         })
     }
-    decrement = (metric) => {        
+
+    decrement = (metric) => {
         this.setState((state) => {
             const count = state[metric] - getMetricMetaInfo(metric).step
 
@@ -32,15 +36,41 @@ export default class AddEntry extends React.Component {
             }
         })
     }
+
     slide = (metric, value) => {
         this.setState(() => ({
             [metric]: value,
         }))
     }
+
     render() {
+        const metaInfo = getMetricMetaInfo()
         return (
             <View>
-                {getMetricMetaInfo('bike').getIcon()}
+                {Object.keys(metaInfo).map((key) => {
+                    const { getIcon, type, ...rest } = metaInfo[key]
+                    const value = this.state[key]
+
+                    return (
+                        <View key={key}>
+                            {getIcon()}
+                            {type === 'slider'
+                                ? <Slider
+                                    value={value}
+                                    onChange={(value) => this.slide(key, value)}
+                                    {...rest}
+                                  />
+                                : <Steppers
+                                    value={value}
+                                    onIncrement={() => this.increment(key)}
+                                    onDecrement={() => this.decrement(key)}
+                                    {...rest}
+                                  /> }
+                        </View>
+                    )
+                })}
+
+
             </View>
         )
     }
