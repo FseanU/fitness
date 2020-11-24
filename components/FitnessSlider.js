6638ac3a859430653b16701a880900a2c9133117
Hungchen Fu <sean14xx@gmail.com>
2020-11-24 15:45:34 +0100

    feat: Add style to FitnessSlider & DateHeader

--- components/FitnessSlider.js
+++ components/FitnessSlider.js
@@ -1,21 +1,36 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import Slider from '@react-native-community/slider';
+import { gray } from '../utils/colors'
 
 export default function FitnessSlider({ max, unit, step, value, onChange }) {
     return (
-        <View>
+        <View style={styles.row}>
             <Slider
+              style={{flex: 1}}
               step={step}
               value={value}
               maximumValue={max}
               minimumValue={0}
               onValueChange={onChange}
             />
-            <View>
-              <Text>{value}</Text>
-              <Text>{unit}</Text>
+            <View style={styles.metricCouter}>
+              <Text style={{fontSize: 24, textAlign: 'center'}}>{value}</Text>
+              <Text style={{fontSize: 18, color: gray}}>{unit}</Text>
             </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center',
+  },
+  metricCouter: {
+    width: 85,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
