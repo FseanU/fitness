1bb01125c96fc4450cc5d738820563ff7cc7fcab
Hungchen Fu <sean14xx@gmail.com>
2020-11-25 18:37:01 +0100

    feat: Add History

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ import { View } from 'react-native'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import AddEntry from './components/AddEntry'
+import History from './components/History'
 import reducer from './reducers'
 
 export default function App() {
   return (
     <Provider store={createStore(reducer)}>
       <View style={{flex: 1}}>
-        <AddEntry />
+        <History />
       </View>
     </Provider>
   );
